9dacf2751a144e4851bd1ad95a286811c77e69f3
harshhh044 <harshk20390@gmail.com>
2026-03-26 22:27:29 +0530

    Update AdminDashboard.jsx

--- CodeFlow-DigitalComplaint&GrievanceSystem/Frontend-files/src/pages/admin/AdminDashboard.jsx
+++ CodeFlow-DigitalComplaint&GrievanceSystem/Frontend-files/src/pages/admin/AdminDashboard.jsx
@@ -1,9 +1,3 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import grievanceService from '../../services/grievanceService';
-
-
-    );
-};
-
-export default AdminDashboard;
