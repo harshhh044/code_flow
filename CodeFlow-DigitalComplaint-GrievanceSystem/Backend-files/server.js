0d14d473c4e8960ce1d1cd803a4d0588fa636d3d
harshhh044 <harshk20390@gmail.com>
2026-04-05 13:32:37 +0530

    Update server.js

--- CodeFlow-DigitalComplaint-GrievanceSystem/Backend-files/server.js
+++ CodeFlow-DigitalComplaint-GrievanceSystem/Backend-files/server.js
@@ -12,7 +12,7 @@ app.options('*', cors());
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: ["http://localhost:5173", "http://localhost:3000","https://code-flow-tau-jet.vercel.app"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
